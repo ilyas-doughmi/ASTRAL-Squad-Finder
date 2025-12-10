47779a8c4abac540513691d27339120eb82f9dc9
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-10 22:02:00 +0100

    feat: adding new include for registering a new user and calling the class function register to register him and redirect him to homepage after success

---
+++ Includes/register.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once("../Class/connexion.php");
+require_once("../Class/User.php");
+
+$user = new User;
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"]; 
+    $rank = $_POST["rank"];
+    $pfp_image = $_POST["pfp_image"]; 
+    $game_playing = $_POST["game_playing"];
+
+    $user->registerUser($username, $email, $password, $pfp_image, $game_playing, $rank);
+
+    header("Location: ../success.php");
+    exit();
+}
+
+?>
