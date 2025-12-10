ee8a8e908f4e7ddf84e47c7867680d5c5b2f8a23
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-10 22:18:02 +0100

    feat: adding new register page and direct it to register.php includes to register a new user

---
+++ pages/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="../Includes/register.php" method="POST">
+    <input type="text" placeholder="username" name="username">
+    <input type="email" placeholder="email" name="email">
+    <input type="password" placeholder="password" name="password">
+    <input type="text" placeholder="pfp_image" name="pfp_image">
+    <input type="text" name="game_playing" placeholder="game_playing">
+    <input type="text" name="rank" placeholder="rank">
+    <button type="submit">register</button>
+
+    </form>
+</body>
+</html>
